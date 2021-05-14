96063358303b541a7fc8cab0a9f66ac3710850e3
Adrian Kretowicz <adiix34@gmail.com>
2021-05-14 13:11:32 +0200

    toggle task done

--- js/script.js
+++ js/script.js
@@ -21,32 +21,63 @@
         render();
     };
 
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    };
+
     const render = () => {
         htmlString = ``;
 
         for (task of tasks) {
             htmlString += `
             <li class="list__item">
-                <button class="list__button">${task.done ? "&#x2714;" : ""}</button>
+                <button class="list__button js-doneTaskButton">${task.done ? "&#x2714;" : ""}</button>
                 <span class="list__task${task.done ? " list__task--done" : ""}">${task.content}</span>
-                <button class="list__button list__button--remove">&#128465;</button>
+                <button class="list__button list__button--remove js-removeTaskButton">&#128465;</button>
             </li>
             `;
         };
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeTaskButtons = document.querySelectorAll(".js-removeTaskButton");
+
+        removeTaskButtons.forEach((removeTaskButton, buttonIndex) => {
+            removeTaskButton.addEventListener("click", () => {
+                removeTask(buttonIndex);
+            });
+        });
+
+        const doneTaskButtons = document.querySelectorAll(".js-doneTaskButton")
+
+        doneTaskButtons.forEach((doneTaskButton, buttonIndex) => {
+            doneTaskButton.addEventListener("click", () => {
+
+                toggleTaskDone(buttonIndex);
+
+                render();
+            });
+        });
+
     };
+
 
     const onFormSubmit = (event) => {
         event.preventDefault();
 
-            const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
 
-            if (newTaskContent === "") {
-                return;
-            };
+        if (newTaskContent === "") {
+            return;
+        };
 
-            addNewTask(newTaskContent);
+        addNewTask(newTaskContent);
     };
 
     const init = () => {
@@ -55,7 +86,11 @@
         const form = document.querySelector(".js-form");
 
         form.addEventListener("submit", onFormSubmit);
+
     };
+
+
+
 
 
     init();
